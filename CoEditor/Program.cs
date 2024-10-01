413f618d39f78fc9be77c7868d3aaa9193285473
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-10-01 15:48:52 +0200

    Added application insight and fixed login problems

--- CoEditor/Program.cs
+++ CoEditor/Program.cs
@@ -1,6 +1,7 @@
 using CoEditor.Components;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,46 +14,36 @@ builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration,
 builder.Services.AddAuthorization();
 builder.Services.AddCascadingAuthenticationState();
 
-//Logging
-builder.Services.AddHttpLogging(o => { });
-//builder.Services.AddApplicationInsightsTelemetry();
+//Application Insights
+var connectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions{ ConnectionString = connectionString });
 
 
 var app = builder.Build();
-app.UseCustomLogging();
-app.UseCustomAuthentication();
-app.UseAntiforgery();
-app.UseExceptionHandler("/Error", createScopeForErrors: true);
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.XForwardedHost });
 app.UseStaticFiles();
+app.UseAuthenticationEndpoints();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseAntiforgery();
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();
 
 file static class WebApplicationExtensions
 {
-    public static void UseCustomLogging(this WebApplication app)
-    {
-        app.Use(next => (async context =>
-        {
-            app.Logger.LogInformation("Log Request {} {}", context.Request.Method, context.Request.GetDisplayUrl());
-            await next(context);
-        }));
-
-        //app.UseHttpLogging();
-        app.Use(next => (async context =>
-        {
-            app.Logger.LogInformation("Log Request {} {}", context.Request.Method, context.Request.GetDisplayUrl());
-            await next(context);
-        }));
-        app.Logger.LogInformation("Adding Logs");
-
-    }
-
-    public static void UseCustomAuthentication(this WebApplication app)
+    public static void UseAuthenticationEndpoints(this WebApplication app)
     {
         var authProperties = new AuthenticationProperties { RedirectUri = "/" };
         var endpointBuilder = app.MapGroup("/authentication");
         endpointBuilder.MapPost("/login", () => TypedResults.Challenge(authProperties)).AllowAnonymous();
         endpointBuilder.MapPost("/logout", () => TypedResults.SignOut(authProperties));
     }
-
 }
