eb9ebf481e45cc94435fa05cc9cb9fa35cc0f03b
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-10-25 18:06:06 +0200

    Fixed client side logging

--- CoEditor.Client/Program.cs
+++ CoEditor.Client/Program.cs
@@ -3,4 +3,5 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddWebAssemblyClient(builder.HostEnvironment);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 await builder.Build().RunAsync();
