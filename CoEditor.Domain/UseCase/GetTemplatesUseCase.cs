a313455e7e4e2f567e46ea775698fd6efbede6bc
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-10-25 22:59:16 +0200

    Fix errors in system templates

--- CoEditor.Domain/UseCase/GetTemplatesUseCase.cs
+++ CoEditor.Domain/UseCase/GetTemplatesUseCase.cs
@@ -18,20 +18,20 @@ internal class GetTemplatesUseCase(
         return templates;
     }
 
-
     [SuppressMessage("ReSharper", "UnusedParameter.Local", Justification = "TODO, will be needed for other languages")]
     private static Template[] GetSystemTemplates(Language language)
     {
         //TODO: Fix for other languages
+        //TODO: Fixed GUIDS for system templates
         return
         [
-            new Template { Id = Guid.Empty, Name = "Default", Text = "I want to write {Context:LongText}" },
+            new Template { Id = Guid.NewGuid(), Name = "Default", Text = "I want to write {Context:LongText}" },
             new Template
             {
-                Id = Guid.Empty,
-                Name = "Email Work Collegue",
+                Id = Guid.NewGuid(),
+                Name = "Email Work Colleague",
                 Text =
-                    "I want to write an email to a work collegue named {Name:string}. I know him {Knowledge:select:well,barely}. The tone should be {Tone:select:humble,aggressive,frindly}. The content of the mail consideres {Context:text}"
+                    "I want to write an email to a work colleague named {Name:text}. I know him {Knowledge:select:well,barely}. The tone should be {Tone:select:humble,aggressive,friendly}. The content of the mail considers {Context:longtext}"
             }
         ];
     }
