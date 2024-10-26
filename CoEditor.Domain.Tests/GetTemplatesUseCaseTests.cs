39504cd1970412ca0d518724fea1788aab2109b6
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-10-26 15:00:56 +0200

    Fix pipeline

--- CoEditor.Domain.Tests/GetTemplatesUseCaseTests.cs
+++ CoEditor.Domain.Tests/GetTemplatesUseCaseTests.cs
@@ -12,22 +12,24 @@ public class GetTemplatesUseCaseTests
     public async Task GetTemplatesAsync()
     {
         // Arrange
-        var loggerStub = new Mock<ILogger<GetTemplatesUseCase>>();
-        Template[] templatesStub = [new() { Id = Guid.NewGuid(), Name = "Name", Text = "Text" }];
+        var loggerStub = new Microsoft.Extensions.Logging.Abstractions.NullLoggerFactory().CreateLogger<GetTemplatesUseCase>();
+        var templatesStub = new Template() { Id = Guid.NewGuid(), Name = "Name", Text = "Text" };
         var templateRepositoryMock = new Mock<ITemplateRepository>();
         templateRepositoryMock
             .Setup(x => x.GetTemplatesAsync("testUser", Language.De))
-            .ReturnsAsync(templatesStub);
+            .ReturnsAsync([templatesStub]);
         var templateService = new GetTemplatesUseCase(
             templateRepositoryMock.Object,
-            loggerStub.Object
+            loggerStub
         );
 
         // Act
         var result = await templateService.GetTemplatesAsync("testUser", Language.De);
 
         // Assert
-        Assert.Equal(templatesStub, result);
         templateRepositoryMock.Verify(x => x.GetTemplatesAsync("testUser", Language.De), Times.Once);
+        Assert.Equal(3, result.Length);
+        Assert.Equal(templatesStub, result[0]);
+        //TODO: Check System templates as well
     }
 }
