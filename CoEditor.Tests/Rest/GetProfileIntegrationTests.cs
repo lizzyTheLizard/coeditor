cb0907dab9455c90b4ff45260bb3948e1b226565
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2024-11-13 23:26:32 +0100

    Implemented integration test (not working)

---
+++ CoEditor.Tests/Rest/GetProfileIntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using CoEditor.Components;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace CoEditor.Tests.Rest;
+
+public class GetProfileIntegrationTests(WebApplicationFactory<App> factory) : IClassFixture<WebApplicationFactory<App>>
+{
+    [Fact]
+    public async Task NotLoggdIn_RedirectToAbout()
+    {
+        var client = factory.CreateClient();
+        var response = await client.GetAsync("/api/Profile/Mine/De", TestContext.Current.CancellationToken);
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+    }
+}
